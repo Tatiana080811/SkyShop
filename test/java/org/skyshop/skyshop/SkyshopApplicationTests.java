7827604deae5f744128420fe206b282cfec9058a
Tatiana080811 <Kazantseva2pochta@gmail.com>
2025-10-29 23:11:49 +0700

    дз

---
+++ test/java/org/skyshop/skyshop/SkyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyshop.skyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
